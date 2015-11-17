13803b7b963d1d59b97e18ca1bd9eee5252e24a5
Steven Nunez <steven.nunez@gmail.com>
2015-11-17 13:24:48 -0500

    Rollback Change to serializer

--- app/serializers/api/v1/message_serializer.rb
+++ app/serializers/api/v1/message_serializer.rb
@@ -1,5 +1,5 @@
 class Api::V1::MessageSerializer < ActiveModel::Serializer
   embed :ids, include: true
-  attributes :id, :to, :from, :body, :starred, :archived
+  attributes :id, :to, :from, :body, :starred
   has_one :label
 end
