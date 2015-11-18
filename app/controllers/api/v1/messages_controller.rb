bbdb28481e1da12ece011cd62c8dbe122fee97c5
Steven Nunez <steven.nunez@gmail.com>
2015-11-17 20:40:27 -0500

    Add Support for filters

--- app/controllers/api/v1/messages_controller.rb
+++ app/controllers/api/v1/messages_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::MessagesController < ApplicationController
   def index
-    @messages = Api::V1::Message.all
+    @messages = Api::V1::Message.from_query(params[:filter])
     render json: @messages
   end
 end
--- app/models/api/v1/message.rb
+++ app/models/api/v1/message.rb
@@ -1,3 +1,20 @@
 class Api::V1::Message < ActiveRecord::Base
   belongs_to :label, foreign_key: :api_v1_label_id
+
+  def self.from_query(query)
+    return all unless ["starred", "archived", "inbox"].include?(query)
+    public_send(query)
+  end
+
+  def self.starred
+    where(starred: true)
+  end
+
+  def self.archived
+    where(archived: true)
+  end
+
+  def self.inbox
+    not.archived
+  end
 end
