61dfe30fa9c3f507030662cf3cebf51981058b69
Steven Nunez <steven.nunez@gmail.com>
2015-11-18 00:59:52 -0500

    Update

--- app/controllers/api/v1/messages_controller.rb
+++ app/controllers/api/v1/messages_controller.rb
@@ -3,4 +3,16 @@ class Api::V1::MessagesController < ApplicationController
     @messages = Api::V1::Message.from_query(params[:filter])
     render json: @messages
   end
+
+  def update
+    @message = Api::V1::Message.find(params[:id])
+    @message.update(safe_message)
+    render json: @message
+  end
+
+  private
+
+  def safe_message
+    params.require(:message).permit(:archived, :body, :from, :starred, :archived, :to)
+  end
 end
